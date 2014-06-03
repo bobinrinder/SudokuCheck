98712bc9054ae9d5dbbe7d51d5a3391d5a58f918
bobinrinder <info@freakweb.de>
2014-06-04 07:06:04 +1000

    Version 1.01
    improved comments ;)

--- SudokuCheck.vb
+++ SudokuCheck.vb
@@ -9,16 +9,18 @@
     End Sub
 
     Public Function ReadData() As List(Of Integer)
+        '81 Numbers as you would read text
         Dim mySudokoData As New List(Of Integer)(New Integer() {9, 1, 2, 8, 4, 6, 5, 7, 3, 6, 8, 3, 5, 7, 1, 2, 9, 4, 4, 5, 7, 3, 2, 9, 1, 6, 8, 8, 2, 9, 6, 1, 3, 4, 5, 7, 1, 6, 4, 9, 5, 7, 8, 3, 2, 3, 7, 5, 2, 8, 4, 6, 1, 9, 7, 4, 6, 1, 9, 2, 3, 8, 5, 5, 9, 1, 4, 3, 8, 7, 2, 6, 2, 3, 8, 7, 6, 5, 9, 4, 1})
         Return mySudokoData
     End Function
 
     Public Function CheckSudoko(mySudokoData As List(Of Integer)) As Boolean
 
-        'Define temporary list to check
+        'Define temporary list for nine numbers to check
         Dim myNineTempNumbers As New List(Of Integer)
 
         'Check Rows
+        ' -> We just have to stop and check whenever there are 9 new numbers
         For i As Integer = 0 To 80
             myNineTempNumbers.Add(mySudokoData(i))
             If myNineTempNumbers.Count = 9 AndAlso Not CheckNineNumbers(myNineTempNumbers) Then
@@ -29,6 +31,7 @@
         Next
 
         'Check Columns
+        ' -> We are getting each number of a column through incrementing by 9
         Dim j As Integer = 0
         For i As Integer = 0 To 8
             While myNineTempNumbers.Count <> 9
@@ -43,7 +46,8 @@
             End If
         Next
 
-        'Check Quarters
+        'Check Quadrats
+        ' -> We basically just get three numbers of a column and do that three times
         For i As Integer = 0 To 54 Step 27
             For k As Integer = 0 To 2
                 myNineTempNumbers.Add(mySudokoData(i + k))
